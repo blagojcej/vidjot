ddac67bb86c16464083668d2e75eba5d1e0b53c7
Blagojce Jovanoski <blagojcejovanoski@gmail.com>
2018-02-05 15:52:22 +0100

    Added registration funcionality

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -10,8 +11,8 @@ const session = require('express-session');
 const app = express();
 
 //Load routes
-const ideas=require('./routes/ideas');
-const users=require('./routes/users');
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
 
 //Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
@@ -30,8 +31,11 @@ app.set('view engine', 'handlebars');
 //Body parser middleware
 app.use(bodyParser.urlencoded({
     extended: false
-}))
-app.use(bodyParser.json())
+}));
+app.use(bodyParser.json());
+
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Method override middleware
 app.use(methodOverride('_method'))
